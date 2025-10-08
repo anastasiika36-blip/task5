e94867298da6beae6d1ec3d365d8ecf0ede2e5d6
79826 <79826@LAPTOP-7TRURQ14>
2025-10-08 21:02:29 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = F(2, 3) + F(5, 7) + F(4, 6);
+        Console.WriteLine($"x = {x:F3}");
+    }
+
+    static double F(double a, double b)
+    {
+        return (Math.Exp(a) - Math.Exp(-a)) / (Math.Exp(b) + Math.Exp(-b));
+    }
+}
